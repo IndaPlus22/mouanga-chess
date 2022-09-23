22305566f377307f41380f98c2ef3ab6b753a4e5
salticecream <shortgolind@gmail.com>
2022-09-23 23:51:57 +0200

    add notes in notes and lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub fn new_game() -> Game {
+    let mut position = "RNBQKBNRPPPPPPPPEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEpppppppprnbqkbnr";   //  first character is a1, then a2, a3 ... 9th character is b1, etc
+    let mut bool white_to_move = true;
 
 #[cfg(test)]
 mod tests {
@@ -12,3 +12,28 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+
+
+/* Functions to add:
+get_rook_moves(<true/false>) - returns legal white rook moves if true, otherwise returns legal black rook moves
+                               add this for every piece
+
+print_board() - prints 8 rows like the following:       [ ]  [R]  [Q]  [p]  [ ]  [p]  [K]  [ ]
+
+                                                        [ ]  [R]  [Q]  [p]  [ ]  [p]  [K]  [ ]
+
+                                                        [ ]  [R]  [Q]  [p]  [ ]  [p]  [K]  [ ]
+
+                                                        [ ]  [R]  [Q]  [p]  [ ]  [p]  [K]  [ ]
+
+                                                        [ ]  [R]  [Q]  [p]  [ ]  [p]  [K]  [ ]
+
+                                                        [ ]  [R]  [Q]  [p]  [ ]  [p]  [K]  [ ]
+
+                                                        [ ]  [R]  [Q]  [p]  [ ]  [p]  [K]  [ ]
+
+                                                        [ ]  [R]  [Q]  [p]  [ ]  [p]  [K]  [ ]
+
+
+
